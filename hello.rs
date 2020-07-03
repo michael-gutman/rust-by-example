e2ff76266b2fc94ed77de1697a5a96a57c4b34d5
Michael Gutman <michaelgutman@hotmail.com>
2020-07-03 18:48:45 -0400

    hello world

---
+++ hello.rs
@@ -0,0 +1,8 @@
+// Rust uses a main function as an entry point
+fn main() {
+    // Simple printing syntax, hurray!
+    println!("Hello World!");
+    // println! is a macro, not a function. We'll get to that later
+    println!("Im a Rustacean!");
+    // don't forget your semicolons...
+}
